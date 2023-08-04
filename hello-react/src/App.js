9687fb67b0798e038f0c2c51e3ff4e8f0e0339ab
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2023-08-04 19:34:04 +0900

    3-3. props(함수형 컴포넌트)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,7 +4,11 @@ import React from 'react';
 import MyComponent from './MyComponent.js';
 
 const App = () => {
-  return <MyComponent />;
+  return (
+    <MyComponent name={'React'} favoriteNumber={1}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
