73f5e13dacf2ac43f62b0e55e61e4a3acbdc35b5
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2023-08-04 16:21:01 +0900

    2장: JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="react">{name}</div>
+      // 이런 주석이나 /* 이런 주석은 따잇 당한다. */
+      <input />
+    </>
   );
 }
 
