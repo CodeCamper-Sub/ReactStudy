8d49b75482b6c599e0a7efb1a7b0f19fe4d22a55
Eric-HanGyeongSoo <gksrudtn99@naver.com>
2023-08-07 12:54:13 +0900

    4. 이벤트 핸들링 (클래스 컴포넌트)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 
 import React from 'react';
-import Say from './Say';
+import EventPractice from './EventPractice';
 
 const App = () => {
-  return <Say />;
+  return <EventPractice />;
 };
 
 export default App;
